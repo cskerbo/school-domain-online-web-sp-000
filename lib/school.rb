2e81163eeef9168d60b216bd675bc79f89a4735c
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-25 16:08:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+  attr_accessor :name, :student, :grade, :roster
+
+
+  def initialize(name)
+    @name = name
+  end
+
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+  end
+end
